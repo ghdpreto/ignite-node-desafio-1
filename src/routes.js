b1de4c8382c9daabb3a8358e516d82c3f82fad2f
Gustavo Henrique <ghdpreto@gmail.com>
2023-05-06 22:23:38 -0300

    feat: atualizar task

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import {Database} from './database.js'
 import { randomUUID } from "node:crypto"
 import { schemaCriarTaskBody } from "./utils/schema-criar-task-body.js"
+import { schemaAtualizaTaskBody } from "./utils/schema-atualizar-task-body.js"
 import { buildRoutePath } from "./utils/build-route-path.js"
 
 /**
@@ -26,10 +27,6 @@ export const routes = [
                 schemaCriarTaskBody(req.body)
 
                 const { title, description } = req.body
-
-                if (!title || !description) {
-                    return res.writeHead(404).end({ mensagem: 'Campos obrigatórios: title, description, body' })
-                }
 
                 const novaTask = {
                     id: randomUUID(),
@@ -103,6 +100,38 @@ export const routes = [
             database.update('tasks', id, { ...existTask })
 
             return res.writeHead(204).end()
+        }
+    },
+
+    {
+        method: 'PUT',
+        url: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            try {
+                schemaAtualizaTaskBody(req.body)
+                const { id } = req.params
+                const { title, description } = req.body
+
+                const existTask = database.selectById('tasks', id)
+
+                if (!existTask) return res.writeHead(404).end()
+
+
+                if (title) {
+                    existTask.title = title
+                }
+
+                if (description) {
+                    existTask.description = description
+                }
+
+                database.update('tasks', id, { ...existTask })
+
+                return res.writeHead(204).end()
+
+            } catch (error) {
+                return res.writeHead(400).end(JSON.stringify({ mensagem: error.message }))
+            }
         }
     },
 ]
